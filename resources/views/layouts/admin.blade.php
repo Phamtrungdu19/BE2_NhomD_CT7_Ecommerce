d0e08e757ee7a13189946127544c1d812dee1ee7
Phamtrungdu19 <19211tt3064@mail.tdc.edu.vn>
2023-05-15 17:35:41 +0700

    create

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('admin/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('admin/vendors/base/vendor.bundle.base.css')}}">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <link rel="stylesheet" href="{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('admin/css/style.css')}}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{ asset('admin/images/favicon.png')}}" />
+
+
+</head>
+
+<body>
+    <div class="container-srcoller">
+        @include('layouts.inc.admin.navbar')
+
+        <div class="container-fluid page-body-wrapper">
+            @include('layouts.inc.admin.sidebar')
+
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    @yield('content')
+
+                    <div class="row">
+                        <div class="col-md-12 grid-margin">
+                            <div class="d-flex justify-content-between flex-wrap">
+                                <div class="d-flex align-items-end flex-wrap">
+                                    <div class="me-md-3 me-xl-5">
+                                        <h2>Welcome back,</h2>
+                                        <p class="mb-md-0">Your analytics dashboard template.</p>
+                                    </div>
+                                    <div class="d-flex">
+                                        <i class="mdi mdi-home text-muted hover-cursor"></i>
+                                        <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp;Dashboard&nbsp;/&nbsp;</p>
+                                        <p class="text-primary mb-0 hover-cursor">Analytics</p>
+                                    </div>
+                                </div>
+                                <div class="d-flex justify-content-between align-items-end flex-wrap">
+                                    <button type="button"
+                                        class="btn btn-light bg-white btn-icon me-3 d-none d-md-block ">
+                                        <i class="mdi mdi-download text-muted"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
+                                        <i class="mdi mdi-clock-outline text-muted"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
+                                        <i class="mdi mdi-plus text-muted"></i>
+                                    </button>
+                                    <button class="btn btn-primary mt-2 mt-xl-0">Generate report</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- plugins:js -->
+    <script src="{{ asset('vendors/base/vendor.bundle.base.js')}}"></script>
+    <!-- Plugin js for this page-->
+    <script src="{{ asset('vendors/chart.js/Chart.min.js')}}"></script>
+    <script src="{{ asset('vendors/datatables.net/jquery.dataTables.js')}}"></script>
+    <script src="{{ asset('vendors/datatables.net-bs4/dataTables.bootstrap4.js')}}"></script>
+    <script src="{{ asset('js/off-canvas.js')}}"></script>
+    <script src="{{ asset('js/hoverable-collapse.js')}}"></script>
+    <script src="{{ asset('js/template.js')}}"></script>
+    <!-- Custom js for this page-->
+    <script src="{{ asset('js/dashboard.js')}}"></script>
+    <script src="{{ asset('js/data-table.js')}}"></script>
+    <script src="{{ asset('js/jquery.dataTables.js')}}"></script>
+    <script src="{{ asset('js/dataTables.bootstrap4.js')}}"></script>
+
+</body>
+
+</html>
