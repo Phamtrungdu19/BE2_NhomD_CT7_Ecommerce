5d7acaf8cce3d4c9856b3a634a013a18b4a998ef
nham1310 <trannham05042002@gmail.com>
2023-05-20 08:23:08 +0700

    products , brand

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -5,14 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Brand extends Model
+class ProductImage extends Model
 {
     use HasFactory;
-    protected $table = 'brands';
+    protected $table = 'product_images';
     protected $fillable = [
-        'name',
-        'slug',
-        'status'
+        'product_id',
+        'image',
+
 
     ];
 }
---
+++ resources/views/admin/products/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.admin');
+
+@section('content');
+
+<div class="row">
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-header">
+        <h4> Products
+          <a href="{{url('admin/products/create')}}" class="btn btn-primary btn-sm float-end">Add Product</a>
+        </h4>
+      </div>
+      <div class="card-body">
+
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
