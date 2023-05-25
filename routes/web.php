9cb46ddcd2aa519b10e679ba3ea1c21f01eed3f9
vanhuy <huy.nguyn1012@gmail.com>
2023-05-25 16:20:31 +0700

    deital-color

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,58 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// s
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Auth::routes();
-Route::get('/',[App\Http\Controllers\Frontend\FrontendController::class,'index' ]);
+Route::get('/', [App\Http\Controllers\Frontend\FrontendController::class, 'index']);
+Route::get('/collections', [App\Http\Controllers\Frontend\FrontendController::class, 'categories']);
+Route::get('collections/{category_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'products']);
+Route::get('/collections/{category_slug}/{product_slug}',[App\Http\Controllers\Frontend\FrontendController::class, 'productView']);
+
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/collection/{category_slug}' , [App\Http\Controllers\FrontendController::class, 'products']);
+
 Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
     Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
 
-    Route::get('/brands', App\Http\Livewire\Admin\Brand\Index::class);
+    //Route::get('/brands', App\Http\Livewire\Admin\Brand\Index::class);
     //Category Router
-    
+    Route::controller(App\Http\Controllers\Admin\SliderController::class)->group(function () {
+        Route::get('/sliders', 'index');
+        Route::get('/sliders/create', 'create');
+        Route::post('/sliders/store', 'store');
+    });
+    Route::controller(App\Http\Controllers\Admin\BrandController::class)->group(function () {
+        Route::get('/brands/', 'index');
+        Route::get('/brands/create', 'create');
+        Route::post('/brands', 'store');
+    });
+
     Route::controller(App\Http\Controllers\Admin\CategoryController::class)->group(function () {
         Route::get('/category', 'index');
         Route::get('/category/create', 'create');
         Route::post('/category', 'store');
         Route::get('/category/{category}/edit', 'edit');
         Route::put('/category/{category}', 'update');
-        
+    });
+
+    Route::controller(App\Http\Controllers\Admin\ProductController::class)->group(function () {
+        Route::get('/products', 'index');
+        Route::get('/products/create', 'create');
+        Route::post('/products', 'store');
+        Route::get('/products/{product}/edit', 'edit');
+        Route::put('/products/{product}', 'update');
+        Route::get('products/{id}/delete', 'destroy');
+        Route::get('product-image/{product_image_id}/delete', 'destroyImage');
+    });
+    Route::controller(App\Http\Controllers\Admin\ColorController::class)->group(function () {
+        Route::get('/colors', 'index');
+        Route::get('/colors/create', 'create');
+        Route::post('/colors/create', 'store');
+        Route::get('/colors/{color}/edit', 'edit');
+        Route::put('/colors/{color}/edit', 'update');
+        Route::get('/colors/{color}/delete', 'destroy');
     });
 });
