c96d1927585460414f0cde3998d0490c165e8dae
vanhuy <huy.nguyn1012@gmail.com>
2023-05-25 10:46:18 +0700

    app

--- routes/web.php
+++ routes/web.php
@@ -14,31 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// s
 Auth::routes();
-
+Route::get('/',[App\Http\Controllers\Frontend\FrontendController::class,'index' ]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+Route::get('/collection/{category_slug}' , [App\Http\Controllers\FrontendController::class, 'products']);
 Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
     Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
 
     Route::get('/brands', App\Http\Livewire\Admin\Brand\Index::class);
     //Category Router
-
+    
     Route::controller(App\Http\Controllers\Admin\CategoryController::class)->group(function () {
         Route::get('/category', 'index');
         Route::get('/category/create', 'create');
         Route::post('/category', 'store');
         Route::get('/category/{category}/edit', 'edit');
         Route::put('/category/{category}', 'update');
-    });
-
-    Route::controller(App\Http\Controllers\Admin\ProductController::class)->group(function () {
-        Route::get('/products', 'index');
-        Route::get('/products/create', 'create');
-        Route::post('/products', 'store');
+        
     });
 });
