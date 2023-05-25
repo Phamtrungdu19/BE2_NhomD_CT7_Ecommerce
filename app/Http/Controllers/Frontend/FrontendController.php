fe89d33b5e4924f096999a41961b766d30e245e4
Phamtrungdu19 <19211tt3064@mail.tdc.edu.vn>
2023-05-25 14:28:29 +0700

    detail-product

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -16,6 +16,31 @@ class FrontendController extends Controller
    }
    public function categories()
    {
-      return view('frontend.collections.category.index');
+      $categories = Category::where('status', '0')->get();
+      return view('frontend.collections.category.index', compact('categories'));
+   }
+   public function products($category_slug)
+   {
+      $category = Category::where('slug', $category_slug)->first();
+      if ($category) {
+         $products = $category->products()->get();
+         return view('frontend.collections.products.index', compact('products', 'category'));
+      } else {
+         return redirect()->back();
+      }
+   }
+   public function productView($category_slug, $product_slug)
+   {
+      $category = Category::where('slug', $category_slug)->first();
+      if ($category) {
+         $product = $category->products()->where('slug', $product_slug)->where('status', '0')->first();
+         if ($product) {
+            return view('frontend.collections.products.view', compact('product', 'category'));
+         } else {
+            return redirect()->back();
+         }
+      } else {
+         return redirect()->back();
+      }
    }
 }
