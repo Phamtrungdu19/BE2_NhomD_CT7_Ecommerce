aeb0689a4c80b12200ec9706aaf075a741164029
nham1310 <trannham05042002@gmail.com>
2023-05-27 00:09:04 +0700

    cate delete

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,52 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('cart', [App\Http\Controllers\Frontend\CartController::class, 'index']);
+});
+
+Route::controller(App\Http\Controllers\Frontend\FrontendController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/collections', 'categories');
+    Route::get('/collections/{category_slug}', 'products');
+    Route::get('collections/{category_slug}/{product_slug}', 'productView');
+    Route::get('/new-arrival', 'newArrival');
+
+
+    Route::get('/search', 'searchProduct');
+});
+// Route::get('/', [App\Http\Controllers\Frontend\FrontendController::class, 'index']);
+// Route::get('/collections', [App\Http\Controllers\Frontend\FrontendController::class, 'categories']);
+// Route::get('collections/{category_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'products']);
+
+// Route::get('collections/{category_slug}/{product_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'productView']);
+
+
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
     Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
 
-    Route::get('/brands', App\Http\Livewire\Admin\Brand\Index::class);
+    //Route::get('/brands', App\Http\Livewire\Admin\Brand\Index::class);
     //Category Router
+    Route::controller(App\Http\Controllers\Admin\SliderController::class)->group(function () {
+        Route::get('/sliders', 'index');
+        Route::get('/sliders/create', 'create');
+        Route::post('/sliders/store', 'store');
+    });
+    Route::controller(App\Http\Controllers\Admin\BrandController::class)->group(function () {
+        Route::get('/brands', 'index');
+        Route::get('/brands/create', 'create');
+        Route::post('/brands', 'store');
+    });
 
     Route::controller(App\Http\Controllers\Admin\CategoryController::class)->group(function () {
         Route::get('/category', 'index');
@@ -34,6 +67,7 @@ Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
         Route::post('/category', 'store');
         Route::get('/category/{category}/edit', 'edit');
         Route::put('/category/{category}', 'update');
+        Route::get('/category/{category}/delete', 'destroy');
     });
 
     Route::controller(App\Http\Controllers\Admin\ProductController::class)->group(function () {
@@ -42,6 +76,16 @@ Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
         Route::post('/products', 'store');
         Route::get('/products/{product}/edit', 'edit');
         Route::put('/products/{product}', 'update');
+        Route::get('products/{id}/delete', 'destroy');
         Route::get('product-image/{product_image_id}/delete', 'destroyImage');
+    });
+
+    Route::controller(App\Http\Controllers\Admin\ColorController::class)->group(function () {
+        Route::get('/colors', 'index');
+        Route::get('/colors/create', 'create');
+        Route::post('/colors/create', 'store');
+        Route::get('/colors/{color}/edit', 'edit');
+        Route::put('/colors/{color}/edit', 'update');
+        Route::get('/colors/{color}/delete', 'destroy');
     });
 });
